0b4d8730227c6b740a0fbe5e0f8f07abe7f74e73
rotamo11 <rotamo@yahoo.com>
2026-09-21 10:21:08 -0400

    Set page config layout to wide

--- pages/chat.py
+++ pages/chat.py
@@ -7,6 +7,8 @@ URL = st.secrets["SUPABASE_URL"]
 KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(URL, KEY)
 
+st.set_page_config(layout="wide")
+
 # --- CUSTOM SIDEBAR CONFIGURATION ---
 with st.sidebar:
     # 1. Main Game Mode Selector
@@ -99,7 +101,6 @@ with header_col2:
 
 st.markdown("---")
 
-st.set_page_config(layout="wide")
 st.subheader("Locker Room Chat")
 
 if 'user' not in st.session_state or not st.session_state.user:
