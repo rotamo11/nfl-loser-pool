dd28dab8805296241075904dc1d7487a5ae9f6b7
rotamo11 <rotamo@yahoo.com>
2026-09-21 16:13:37 -0400

    Fix header display for selected week label

--- pages/admin.py
+++ pages/admin.py
@@ -144,7 +144,7 @@ with header_col2:
         f"""
         <div style="display: flex; align-items: flex-end; height: 85px; padding-bottom: 5px;">
             <h1 style="margin:0; font-weight:900; font-size:32px; letter-spacing:-1px;">
-                2026 NFL Loser Pool &bull; {game_mode} &bull; Week {selected_week_label}
+                2026 NFL Loser Pool &bull; {game_mode} &bull; {selected_week_label}
             </h1>
         </div>
         """
